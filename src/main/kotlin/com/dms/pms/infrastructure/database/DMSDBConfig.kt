4cb61c0cdc612a07b76c9a7de8820602af985644
smoothbear <kjbin0420@naver.com>
2021-08-22 23:35:41 +0900

    Add DMSDBConfig for following #1

---
+++ src/main/kotlin/com/dms/pms/infrastructure/database/DMSDBConfig.kt
@@ -0,0 +1,51 @@
+package com.dms.pms.infrastructure.database
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import javax.persistence.EntityManagerFactory
+import javax.sql.DataSource
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    entityManagerFactoryRef = "dmsEntityManager",
+    transactionManagerRef = "dmsTransactionManager",
+    basePackages = ["com.dms.pms.domain.point", "com.dms.pms.domain.stay", "com.dms.pms.domain.student"]
+)
+class DMSDBConfig {
+
+    @Bean(name = ["dmsDataSource"])
+    @ConfigurationProperties(prefix = "datasource.dms")
+    fun dataSource(): DataSource {
+        return DataSourceBuilder.create().build()
+    }
+
+    @Bean(name = ["dmsEntityManagerFactory"])
+    fun localContainerEntityManagerFactoryBean(
+        builder: EntityManagerFactoryBuilder,
+        @Qualifier("dmsDataSource") dataSource: DataSource
+    ): LocalContainerEntityManagerFactoryBean {
+        return builder
+            .dataSource(dataSource)
+            .packages("com.dms.pms.domain.point", "com.dms.pms.domain.stay", "com.dms.pms.domain.student")
+            .persistenceUnit("dms")
+            .build()
+    }
+
+    @Bean(name = ["dmsTransactionManager"])
+    fun dmsTransactionManager(
+        @Qualifier("dmsEntityManagerFactory")
+        entityManagerFactory: EntityManagerFactory
+    ): PlatformTransactionManager {
+        return JpaTransactionManager(entityManagerFactory)
+    }
+}
