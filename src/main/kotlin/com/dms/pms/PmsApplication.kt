56f77cb0b0146d1910a0e7abff4dd72aa1bfdb67
smoothbear <kjbin0420@naver.com>
2021-08-22 21:43:54 +0900

    Construct project structure

---
+++ src/main/kotlin/com/dms/pms/PmsApplication.kt
@@ -0,0 +1,11 @@
+package com.dms.pms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PmsApplication
+
+fun main(args: Array<String>) {
+    runApplication<PmsApplication>(*args)
+}
