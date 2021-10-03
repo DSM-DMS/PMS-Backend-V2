9be1974f4e90633ac6247e0460dba9162edeb53e
smoothbear <kjbin0420@naver.com>
2021-10-03 15:17:55 +0900

    Fix data-redis style entity

--- src/main/kotlin/com/dms/pms/domain/auth/domain/cache/ResetPassword.kt
+++ src/main/kotlin/com/dms/pms/domain/auth/domain/cache/ResetPassword.kt
@@ -1,14 +1,12 @@
 package com.dms.pms.domain.auth.domain.cache
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
-import javax.persistence.Column
-import javax.persistence.Id
 
 @RedisHash(value = "ResetPassword", timeToLive = 60 * 60 * 2)
 class ResetPassword (
     @Id
     var email: String,
 
-    @Column(unique = true)
     var token: String
 )
