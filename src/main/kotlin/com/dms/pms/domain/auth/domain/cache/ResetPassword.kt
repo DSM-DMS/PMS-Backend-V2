68597691878b604c92b16ccef614cb7ea1ee8dc3
smoothbear <kjbin0420@naver.com>
2021-10-03 22:57:11 +0900

    Add indexed in redis entity

--- src/main/kotlin/com/dms/pms/domain/auth/domain/cache/ResetPassword.kt
+++ src/main/kotlin/com/dms/pms/domain/auth/domain/cache/ResetPassword.kt
@@ -2,11 +2,13 @@ package com.dms.pms.domain.auth.domain.cache
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
 
 @RedisHash(value = "ResetPassword", timeToLive = 60 * 60 * 2)
 class ResetPassword (
     @Id
     var email: String,
 
+    @Indexed
     var token: String
 )
