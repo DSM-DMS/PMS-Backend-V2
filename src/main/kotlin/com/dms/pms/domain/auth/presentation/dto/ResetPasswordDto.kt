536a33c7b3bcbcc1096ccaed300c2b0f598e32cb
smoothbear <kjbin0420@naver.com>
2021-10-03 22:51:31 +0900

    Add email validation annotation

--- src/main/kotlin/com/dms/pms/domain/auth/presentation/dto/ResetPasswordDto.kt
+++ src/main/kotlin/com/dms/pms/domain/auth/presentation/dto/ResetPasswordDto.kt
@@ -1,7 +1,12 @@
 package com.dms.pms.domain.auth.presentation.dto
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotNull
+
 class ResetPasswordDto {
     class Request (
+        @Email(message = "Email address is not valid.")
+        @NotNull
         val email: String
     )
 }
