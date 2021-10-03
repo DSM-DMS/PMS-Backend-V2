d2efcac916f0815d2ab0ea82dde6307df99de773
smoothbear <kjbin0420@naver.com>
2021-10-03 20:34:58 +0900

    Remove fetch join

--- src/main/kotlin/com/dms/pms/domain/student/domain/pms/repository/StudentUserRepositoryImpl.kt
+++ src/main/kotlin/com/dms/pms/domain/student/domain/pms/repository/StudentUserRepositoryImpl.kt
@@ -26,7 +26,6 @@ class StudentUserRepositoryImpl : PMSQueryDslRepositorySupport(StudentUser::clas
     override fun findAllStudentsByEmail(email: String): List<Student> {
         return from(student)
             .innerJoin(studentUser).on(student.studentCode.eq(studentUser.studentUserKey.studentCode))
-            .fetchJoin()
             .innerJoin(user).on(studentUser.studentUserKey.email.eq(user.email))
             .where(studentUser.studentUserKey.email.eq(email))
             .orderBy(student.studentNumber.asc())
